e2623a0053870eea2692dea069e1a6284bed449f
Lena Stoever <l_stoever@yahoo.de>
2009-07-02 07:49:57 +0000

    SLAsticControl für Mittelwertsberechnung

--- src/org/trustsoft/slastic/control/SLAsticControl.java
+++ src/org/trustsoft/slastic/control/SLAsticControl.java
@@ -2,14 +2,11 @@ package org.trustsoft.slastic.control;
 
 import java.io.File;
 import java.util.Collection;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import kieker.common.logReader.IMonitoringRecordConsumer;
 import kieker.common.logReader.filesystemReader.FilesystemReader;
 import kieker.common.tools.logReplayer.ReplayDistributor;
 import kieker.loganalysis.LogAnalysisInstance;
 import kieker.loganalysis.datamodel.ExecutionSequence;
-import kieker.loganalysis.datamodel.InvalidTraceException;
 import kieker.loganalysis.plugins.DependencyGraphPlugin;
 import kieker.loganalysis.recordConsumer.ExecutionSequenceRepositoryFiller;
 import kieker.loganalysis.recordConsumer.MonitoringRecordTypeLogger;
@@ -17,6 +14,7 @@ import kieker.tpmon.core.TpmonController;
 import kieker.tpmon.monitoringRecord.AbstractKiekerMonitoringRecord;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.trustsoft.slastic.control.recordConsumer.ResponseTimeAverageCalculator;
 import org.trustsoft.slastic.control.recordConsumer.ResponseTimePlotter;
 
 /**
@@ -25,30 +23,32 @@ import org.trustsoft.slastic.control.recordConsumer.ResponseTimePlotter;
 public class SLAsticControl {
 
     private static final Log log = LogFactory.getLog(SLAsticControl.class);
+
     private static final TpmonController ctrlInst = TpmonController.getInstance();
+
     private static final IMonitoringRecordConsumer logCons = new IMonitoringRecordConsumer() {
 
-        /** Anonymous consumer class that simply passes all records to the
-         *  controller */
-        public String[] getRecordTypeSubscriptionList() {
-            return null; // consume all types
-        }
+                /** Anonymous consumer class that simply passes all records to the
+                 *  controller */
+                public String[] getRecordTypeSubscriptionList() {
+                    return null; // consume all types
+                }
 
-        public void consumeMonitoringRecord(final AbstractKiekerMonitoringRecord monitoringRecord) {
-            ctrlInst.logMonitoringRecord(monitoringRecord);
-        }
+                public void consumeMonitoringRecord(final AbstractKiekerMonitoringRecord monitoringRecord) {
+                    ctrlInst.logMonitoringRecord(monitoringRecord);
+                }
 
-        public boolean execute() {
-            // do nothing, we are synchronous
-            return true;
-        }
+                public boolean execute() {
+                    // do nothing, we are synchronous
+                    return true;
+                }
 
-        public void terminate() {
-            ctrlInst.terminateMonitoring();
-        }
-    };
+                public void terminate() {
+                    ctrlInst.terminateMonitoring();
+                }
+            };
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         log.info("Hi, this is SLAsticControl");
 
         String inputDir = System.getProperty("inputDir");
@@ -69,28 +69,35 @@ public class SLAsticControl {
         analysisInstance.addConsumer(new MonitoringRecordTypeLogger());
 
         /* Collects all executions */
-        ExecutionSequenceRepositoryFiller seqRepConsumer = new ExecutionSequenceRepositoryFiller();
-        analysisInstance.addConsumer(seqRepConsumer);
+//        ExecutionSequenceRepositoryFiller seqRepConsumer = new ExecutionSequenceRepositoryFiller();
+//        analysisInstance.addConsumer(seqRepConsumer);
 
         /* Dumps response times */
-        ResponseTimePlotter rtPlotter = new ResponseTimePlotter();
-        analysisInstance.addConsumer(rtPlotter);
+//        ResponseTimePlotter rtPlotter = new ResponseTimePlotter();
+//        analysisInstance.addConsumer(rtPlotter);
+        ResponseTimeAverageCalculator rtac = new ResponseTimeAverageCalculator();
+       //IMonitoringRecordConsumer rtDistributorCons = new ReplayDistributor(7, rtac);
+        //analysisInstance.addConsumer(rtDistributorCons);
+
+        
+        
+        analysisInstance.addConsumer(rtac);
 
         /* Replays traces in real time */
-        IMonitoringRecordConsumer rtDistributorCons = new ReplayDistributor(7, logCons);
-        analysisInstance.addConsumer(rtDistributorCons);
-
+       
         analysisInstance.run();
+        
+        
 
-        try {
-            /* Example that plots a dependency graph */
-            /* generate dependency diagram */
-            Collection<ExecutionSequence> seqEnum = seqRepConsumer.getExecutionSequenceRepository().repository.values();
-            DependencyGraphPlugin.writeDotFromExecutionTraces(seqEnum, inputDir + File.separator + "/dependencyGraph.dot", null);
-            log.info("Wrote dependency graph to file " + inputDir + File.separator + "/dependencyGraph.dot");
-        } catch (InvalidTraceException ex) {
-            log.error("Invalid trace", ex);
-        }
+        /* Example that plots a dependency graph */
+        /* generate dependency diagram */
+
+
+//        Collection<ExecutionSequence> seqEnum = seqRepConsumer.getExecutionSequenceRepository().repository.values();
+//        DependencyGraphPlugin.writeDotFromExecutionTraces(seqEnum, inputDir+File.separator+"/dependencyGraph.dot");
+//        log.info("Wrote dependency graph to file " + inputDir+File.separator+"/dependencyGraph.dot");
+
+
 
         log.info("Bye, this was SLAsticControl");
         System.exit(0);
