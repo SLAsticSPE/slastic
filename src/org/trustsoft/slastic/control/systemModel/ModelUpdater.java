79145d8d1c634d4dc32c598e341996748b67e7b8
Lena Stoever <l_stoever@yahoo.de>
2009-09-18 17:35:21 +0000

    ModelUpdater hinzugefügt

---
+++ src/org/trustsoft/slastic/control/systemModel/ModelUpdater.java
@@ -0,0 +1,81 @@
+package org.trustsoft.slastic.control.systemModel;
+
+import java.util.concurrent.ConcurrentSkipListSet;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.trustsoft.slastic.monadapt.monitoringRecord.SLA.SLOMonitoringRecord;
+
+import reconfMM.*;
+
+public class ModelUpdater {
+	
+	private static final Log log = LogFactory.getLog(ModelUpdater.class);
+	private static ReconfigurationModel model;
+	private static ModelUpdater instance;
+	//private final static TreeMap<Integer, ConcurrentSkipListSet<SLOMonitoringRecord>> map = new TreeMap<Integer, ConcurrentSkipListSet<SLOMonitoringRecord>>();
+	
+	private  ModelUpdater(ReconfigurationModel reconfigurationModel){
+		model = reconfigurationModel;
+		this.initSet();
+	}
+	
+	private void initSet() {
+		for(int i = 0; i< model.getComponents().size(); i++){
+			for(int k = 0; k < model.getComponents().get(i).getServices().size(); k++){
+				//It is important not to use a Set of Longs, because of the possibility of equal values of response times
+				ConcurrentSkipListSet<SLOMonitoringRecord> list = new ConcurrentSkipListSet<SLOMonitoringRecord>();
+				model.getComponents().get(i).getServices().get(k).setResponseTimes(list);				
+			}
+		}
+		
+	}
+
+	public static void initModel(ReconfigurationModel model){
+		if(instance == null){
+			instance = new ModelUpdater(model);
+		}else{
+			log.info("ModelUpdater is already initialized");
+		}
+	}
+	
+	public synchronized static ModelUpdater getInstance(){
+		if(instance==null){
+			log.error("ModelUpdater is not yet initialized");
+			return null;
+		}else{
+			return instance;
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static void updateModel(SLOMonitoringRecord newRecord, SLOMonitoringRecord oldRecord){
+		int serviceID = newRecord.serviceId;
+		for(int i = 0; i< model.getComponents().size();i++){
+			for(int k = 0; k<model.getComponents().get(i).getServices().size(); k++){
+				Service service = model.getComponents().get(i).getServices().get(k);
+				if(service.getServiceID() == serviceID){
+					((ConcurrentSkipListSet<SLOMonitoringRecord>)service.getResponseTimes()).add(newRecord);
+				}
+				if(oldRecord != null){
+					if(service.getServiceID() == oldRecord.serviceId){
+						((ConcurrentSkipListSet<SLOMonitoringRecord>)service.getResponseTimes()).remove(oldRecord);
+						oldRecord = null;
+					}
+				}
+			}
+		}
+	}
+	
+	public static ConcurrentSkipListSet<SLOMonitoringRecord> getResponseTimes(int serviceID){
+		for(int i = 0; i< model.getComponents().size();i++){
+			for(int k = 0; k< model.getComponents().get(i).getServices().size(); k++){
+				if(model.getComponents().get(i).getServices().get(k).getServiceID() == serviceID){
+					return (ConcurrentSkipListSet<SLOMonitoringRecord>) model.getComponents().get(i).getServices().get(k).getResponseTimes();
+				}
+			}
+		}
+		log.error("No entry for ServiceID:"+serviceID+" found");
+		return null;
+	}
+}
