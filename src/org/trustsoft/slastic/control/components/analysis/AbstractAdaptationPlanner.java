6b54dffb4aacf56d4c414b063f4cb6c60768fbe4
Andre van Hoorn <avh@informatik.uni-kiel.de>
2010-01-06 12:40:26 +0000

    refactoring

---
+++ src/org/trustsoft/slastic/control/components/analysis/AbstractAdaptationPlanner.java
@@ -0,0 +1,9 @@
+package org.trustsoft.slastic.control.components.analysis;
+
+/**
+ *
+ * @author Andre van Hoorn
+ */
+public class AbstractAdaptationPlanner {
+
+}
---
+++ src/org/trustsoft/slastic/control/components/analysis/AbstractAnalysisSubComponent.java
@@ -0,0 +1,12 @@
+package org.trustsoft.slastic.control.components.analysis;
+
+
+import org.trustsoft.slastic.control.components.AbstractSLAsticComponent;
+
+/**
+ *
+ * @author Andre van Hoorn
+ */
+public abstract class AbstractAnalysisSubComponent extends AbstractSLAsticComponent {
+
+}
