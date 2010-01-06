2f8c29b438d36d37f1d975417caec7da8ccd6c54
Andre van Hoorn <avh@informatik.uni-kiel.de>
2010-01-06 12:42:55 +0000

    refactoring

--- src/org/trustsoft/slastic/control/components/analysis/AbstractPerformancePredictor.java
+++ src/org/trustsoft/slastic/control/components/analysis/AbstractPerformancePredictor.java
@@ -4,6 +4,6 @@ package org.trustsoft.slastic.control.components.analysis;
  *
  * @author Andre van Hoorn
  */
-public abstract class AbstractAdaptationPlanner extends AbstractAnalysisSubComponent {
+public abstract class AbstractPerformancePredictor extends AbstractAnalysisSubComponent {
 
 }
--- src/org/trustsoft/slastic/control/components/analysis/AbstractWorkloadForecaster.java
+++ src/org/trustsoft/slastic/control/components/analysis/AbstractWorkloadForecaster.java
@@ -4,6 +4,6 @@ package org.trustsoft.slastic.control.components.analysis;
  *
  * @author Andre van Hoorn
  */
-public abstract class AbstractAdaptationPlanner extends AbstractAnalysisSubComponent {
+public abstract class AbstractWorkloadForecaster extends AbstractAnalysisSubComponent {
 
 }
