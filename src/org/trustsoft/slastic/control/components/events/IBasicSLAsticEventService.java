935e68d83ccc786261722c6103d235a6915b58fc
Andre van Hoorn <avh@informatik.uni-kiel.de>
2010-01-07 16:47:26 +0000

    refactoring

---
+++ src/org/trustsoft/slastic/control/components/events/IBasicSLAsticEventService.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.trustsoft.slastic.control.components.events;
+
+/**
+ *
+ * @author Andre van Hoorn
+ */
+public interface IBasicSLAsticEventService {
+    public void sendEvent (ISLAsticEvent ev);
+}
