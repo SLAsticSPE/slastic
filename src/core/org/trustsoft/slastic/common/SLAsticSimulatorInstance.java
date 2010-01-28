2e9a63753dd0477ab6ce52149da66a01d94351c3
Andre van Hoorn <avh@informatik.uni-kiel.de>
2010-01-28 13:06:22 +0000

    restructuring

--- src/core/org/trustsoft/slastic/common/SLAsticSimulatorInstance.java
+++ src/core/org/trustsoft/slastic/common/SLAsticSimulatorInstance.java
@@ -10,12 +10,18 @@ import org.apache.commons.logging.LogFactory;
  * @author Andre van Hoorn
  */
 public class SLAsticSimulatorInstance {
-	private static final Log log = LogFactory.getLog(SLAsticSimulatorInstance.class);
+    private static final Log log = LogFactory.getLog(SLAsticSimulatorInstance.class);
+
+    //private SimulationController simCtrl;
+    //    SimPlanReceiver // delegiert an ctrl.
+
+    
 
     /** No construction via default constructor. */
     private SLAsticSimulatorInstance(){}
 
     public SLAsticSimulatorInstance(Properties props){
+
     }
 
     public void run(){
