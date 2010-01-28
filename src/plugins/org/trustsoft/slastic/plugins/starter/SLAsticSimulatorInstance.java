a0783229feed58df4b290e004c0fdd97763d5080
Andre van Hoorn <avh@informatik.uni-kiel.de>
2010-01-28 15:32:22 +0000

    integrating simulation configuration file

--- src/plugins/org/trustsoft/slastic/plugins/starter/SLAsticSimulatorInstance.java
+++ src/plugins/org/trustsoft/slastic/plugins/starter/SLAsticSimulatorInstance.java
@@ -14,6 +14,15 @@ public class SLAsticSimulatorInstance {
     private static final Log log = LogFactory.getLog(SLAsticSimulatorInstance.class);
 
     private Properties props;
+    private static final String PROP_NAME_PREFIX="slastic.simulation";
+    private static final String PROP_NAME_FSREADER_INPUTDIR=PROP_NAME_PREFIX+".fsreader.inputdir";
+    private static final String PROP_NAME_FSREADER_RTMODE=PROP_NAME_PREFIX+".fsreader.rtmode";
+    private static final String PROP_NAME_FSREADER_RTNUMTHREADS=PROP_NAME_PREFIX+".fsreader.rtnumthreads";
+
+    private String fsReaderInputDir;
+    private boolean fsReaderRTMode = false;
+    private int fsReaderRTNumThreads = -1;
+
 
     //private SimulationController simCtrl;
     //    SimPlanReceiver // delegiert an ctrl.
@@ -23,6 +32,26 @@ public class SLAsticSimulatorInstance {
 
     public SLAsticSimulatorInstance(final Properties props){
         this.props = props;
+        this.initFromProps();
+    }
+
+    private void initFromProps() throws IllegalArgumentException{
+        try {
+        this.fsReaderInputDir = props.getProperty(PROP_NAME_FSREADER_INPUTDIR);
+        this.fsReaderRTMode = props.getProperty(PROP_NAME_FSREADER_RTMODE, "").trim().toLowerCase().equals("true");
+        this.fsReaderRTNumThreads = Integer.parseInt(props.getProperty(PROP_NAME_FSREADER_RTNUMTHREADS));
+
+        if (this.fsReaderInputDir==null || this.fsReaderInputDir.length() == 0){
+            throw new IllegalArgumentException("Missing or empty property "+PROP_NAME_FSREADER_INPUTDIR);
+        }
+        if (this.fsReaderRTMode && this.fsReaderRTNumThreads <= 0){
+            throw new IllegalArgumentException("Missing, empty or invalid property "+PROP_NAME_FSREADER_RTNUMTHREADS);
+        }
+
+        } catch (Exception exc){
+            log.error("Init error", exc);
+            throw new IllegalArgumentException("Init error", exc);
+        }
     }
 
     public void run(){
@@ -31,7 +60,8 @@ public class SLAsticSimulatorInstance {
     }
 
     private void test() {
-        FilesystemLogReplayer player = new FilesystemLogReplayer("tmp/tpmon-20100126-154804106/", true, 5);
+        /**  */
+        FilesystemLogReplayer player = new FilesystemLogReplayer(this.fsReaderInputDir, this.fsReaderRTMode, this.fsReaderRTNumThreads);
         player.execute();
     }
 }
