46b6136006e24ba1e4038016b33dd25b5a3c630d
Andre van Hoorn <avh@informatik.uni-kiel.de>
2010-12-02 21:35:40 +0100

    added methods to load int/boolean/String properties

--- src/core/org/trustsoft/slastic/common/util/PropertiesFileUtils.java
+++ src/core/org/trustsoft/slastic/common/util/PropertiesFileUtils.java
@@ -5,6 +5,9 @@ import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Properties;
+
+import kieker.monitoring.core.configuration.ConfigurationProperty;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -18,20 +21,20 @@ public class PropertiesFileUtils {
 
     public static Properties loadPropertiesFile(final String fn) throws IllegalArgumentException {
         InputStream is = null;
-        Properties prop = new Properties();
+        final Properties prop = new Properties();
 
         try {
             is = new FileInputStream(fn);
-            log.info("Loading properties from file '" + fn + "'");
+            PropertiesFileUtils.log.info("Loading properties from file '" + fn + "'");
             prop.load(is);
-        } catch (Exception ex) {
-            log.error("Failed to load properties from file '" + fn + "'", ex);
+        } catch (final Exception ex) {
+            PropertiesFileUtils.log.error("Failed to load properties from file '" + fn + "'", ex);
             throw new IllegalArgumentException("Failed to load properties from file '" + fn + "'", ex);
         } finally {
             try {
                 is.close();
-            } catch (Exception ex) {
-                log.error("Failed to close property input stream", ex);
+            } catch (final Exception ex) {
+                PropertiesFileUtils.log.error("Failed to close property input stream", ex);
             }
         }
         return prop;
@@ -42,18 +45,89 @@ public class PropertiesFileUtils {
 
         try {
             os = new FileOutputStream(fn);
-            log.info("Storing properties to file '" + fn + "'");
+            PropertiesFileUtils.log.info("Storing properties to file '" + fn + "'");
             prop.store(os, null); // no comment
-        } catch (Exception ex) {
-            log.error("Failed to store properties to file '" + fn + "'", ex);
+        } catch (final Exception ex) {
+            PropertiesFileUtils.log.error("Failed to store properties to file '" + fn + "'", ex);
             throw new IllegalArgumentException("Failed to store properties to file '" + fn + "'", ex);
         } finally {
             try {
                 os.close();
-            } catch (Exception ex) {
-                log.error("Failed to close property output stream", ex);
+            } catch (final Exception ex) {
+                PropertiesFileUtils.log.error("Failed to close property output stream", ex);
             }
         }
         return prop;
     }
+    
+	/**
+	 * 
+	 * @param props
+	 * @param propertyName
+	 * @param defaultValue
+	 * @param considerSystemProperties
+	 * @return
+	 */
+	public static boolean loadBooleanConfigurationProperty(
+			final Properties props, final ConfigurationProperty property,
+			final boolean considerSystemProperties) {
+		final String stringValue = PropertiesFileUtils
+				.loadStringConfigurationProperty(props, property,
+						considerSystemProperties);
+
+		return Boolean.parseBoolean(stringValue);
+	}
+
+	/**
+	 * 
+	 * @param props
+	 * @param property
+	 * @param considerSystemProperties
+	 * @return
+	 * @throws NumberFormatException
+	 */
+	public static int loadIntConfigurationProperty(final Properties props,
+			final ConfigurationProperty property,
+			final boolean considerSystemProperties)
+			throws NumberFormatException {
+		final String stringValue = PropertiesFileUtils
+				.loadStringConfigurationProperty(props, property,
+						considerSystemProperties);
+
+		return Integer.parseInt(stringValue);
+	}
+
+	/**
+	 * 
+	 * @param props
+	 * @param property
+	 * @param considerSystemProperties
+	 * @return
+	 */
+	public static String loadStringConfigurationProperty(
+			final Properties props, final ConfigurationProperty property,
+			final boolean considerSystemProperties) {
+		String propertyValue;
+		if (considerSystemProperties && property.hasJVMArgument()
+				&& (System.getProperty(property.getJVMArgumentName()) != null)) {
+			/* We use the present virtual machine parameter value */
+			propertyValue = System.getProperty(property.getJVMArgumentName());
+		} else if (property.getPropertyName() != null) {
+			/* we use the value from the properties map */
+			propertyValue = props.getProperty(property.getPropertyName());
+		} else {
+			/* Un-named property with a default value */
+			propertyValue = property.getDefaultValue();
+		}
+
+		if ((propertyValue == null)
+				|| (!property.isAllowEmpty() && propertyValue.isEmpty())) {
+			propertyValue = property.getDefaultValue();
+			PropertiesFileUtils.log.info("Missing value for property '"
+					+ property.getPropertyName() + "' using default value "
+					+ propertyValue);
+		}
+
+		return propertyValue;
+	}
 }
