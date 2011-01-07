97e3fa005616c552771f86881ba4b61693c3c952
Andre van Hoorn <avh@informatik.uni-kiel.de>
2011-01-07 15:48:29 +0100

    added creator for fq name

--- src/plugins/org/trustsoft/slastic/plugins/slasticImpl/model/NameUtils.java
+++ src/plugins/org/trustsoft/slastic/plugins/slasticImpl/model/NameUtils.java
@@ -1,35 +1,50 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * To change this template, choose Tools | Templates and open the template in
+ * the editor.
  */
 
 package org.trustsoft.slastic.plugins.slasticImpl.model;
 
 /**
- *
+ * 
  * @author Andre van Hoorn
  */
 public class NameUtils {
-   /**
-     * Returns an array containing the package name and identifier components
-     * of a fully-qualified name string.
-     *
-     * @param fullyQualifiedName the fully-qualified name to split
-     * @return the array field with index 0 contains the package name,
-     *         the array field with index 1 contains the identifier
-     *
-     */
-    public static String[] splitFullyQualifiedName(final String fullyQualifiedName) {
-        final int idx = fullyQualifiedName.lastIndexOf(".");
-        final String identifier;
-        final String packageName;
-        if (idx == -1) { // no package name
-            packageName = "";
-            identifier = fullyQualifiedName;
-        } else {
-            packageName = fullyQualifiedName.substring(0, idx);
-            identifier = fullyQualifiedName.substring(idx + 1, fullyQualifiedName.length());
-        }
-        return new String[]{packageName, identifier};
-    }
+	/**
+	 * Returns an array containing the package name and identifier components of
+	 * a fully-qualified name string.
+	 * 
+	 * @param fullyQualifiedName
+	 *            the fully-qualified name to split
+	 * @return the array field with index 0 contains the package name, the array
+	 *         field with index 1 contains the identifier
+	 * 
+	 */
+	public static String[] splitFullyQualifiedName(
+			final String fullyQualifiedName) {
+		final int idx = fullyQualifiedName.lastIndexOf(".");
+		final String identifier;
+		final String packageName;
+		if (idx == -1) { // no package name
+			packageName = "";
+			identifier = fullyQualifiedName;
+		} else {
+			packageName = fullyQualifiedName.substring(0, idx);
+			identifier =
+					fullyQualifiedName.substring(idx + 1,
+							fullyQualifiedName.length());
+		}
+		return new String[] { packageName, identifier };
+	}
+
+	public static String createFQName(final String packageName,
+			final String name) {
+		final StringBuilder strB = new StringBuilder();
+
+		if ((packageName != null) && !packageName.isEmpty()) {
+			strB.append(packageName).append(".");
+		}
+		strB.append(name);
+		return strB.toString();
+	}
 }
