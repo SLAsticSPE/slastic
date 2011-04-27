d4f3cca1aed84f219eb25d526f8c601df81d7aae
Andre van Hoorn <avh@informatik.uni-kiel.de>
2011-04-27 10:08:35 +0200

    working on r facade

--- src/plugins/org/trustsoft/slastic/plugins/util/REngineFacade.java
+++ src/plugins/org/trustsoft/slastic/plugins/util/REngineFacade.java
@@ -5,14 +5,9 @@ import java.awt.Frame;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.concurrent.Callable;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.rosuda.JRI.REXP;
@@ -28,7 +23,7 @@ public class REngineFacade {
     private final ExecutorService jobQueue =
             Executors.newSingleThreadExecutor();
     private static final Log log = LogFactory.getLog(REngineFacade.class);
-    private final static REngineFacade INSTANCE = new REngineFacade();
+
     private final Rengine rEngine;
 
     private REngineFacade() throws IllegalArgumentException {
@@ -37,18 +32,18 @@ public class REngineFacade {
 
     private final Rengine initREngine() throws IllegalArgumentException {
         if (!Rengine.versionCheck()) {
-            log.error("R Version mismatch - Java files don't match library version.");
+            REngineFacade.log.error("R Version mismatch - Java files don't match library version.");
             throw new IllegalArgumentException("R Version mismatch - Java files don't match library version.");
         }
-        log.info("Creating Rengine (with arguments)");
+        REngineFacade.log.info("Creating Rengine (with arguments)");
 
-        Rengine re = new Rengine(
+        final Rengine re = new Rengine(
                 new String[]{"--vanilla"},
                 false,
                 new TextConsole());
-        log.info("Rengine created, waiting for R");
+        REngineFacade.log.info("Rengine created, waiting for R");
         if (!re.waitForR()) {
-            log.error("Cannot load R");
+            REngineFacade.log.error("Cannot load R");
             throw new IllegalArgumentException("Cannot load R");
         }
 
@@ -56,9 +51,19 @@ public class REngineFacade {
     }
 
     public static final REngineFacade getInstance() {
-        return INSTANCE;
+        return LazyHolder.INSTANCE;
     }
 
+	/**
+	 * SINGLETON
+	 */
+	private final static class LazyHolder {
+		static {
+		    INSTANCE = new REngineFacade();
+		}
+		private final static REngineFacade INSTANCE;
+	}
+    
     /** Terminates the REngineFacade. */
     public final void end() {
         this.jobQueue.shutdown();
@@ -70,7 +75,7 @@ public class REngineFacade {
      * This method returns immediately. The result is passed to the
      * caller via the callback handler.
      */
-    protected final void rEvalAsync(String rCmd, IREvalCallbackHandler resultHandler) {
+    protected final void rEvalAsync(final String rCmd, final IREvalCallbackHandler resultHandler) {
         this.jobQueue.submit(new RCallable(rCmd, resultHandler));
     }
 
@@ -79,11 +84,11 @@ public class REngineFacade {
      *
      * @return the result
      */
-    protected final REXP rEvalSync(String rCmd) throws REngineFacadeEvalException {
+    protected final REXP rEvalSync(final String rCmd) throws REngineFacadeEvalException {
         try {
             return this.jobQueue.submit(new RCallable(rCmd)).get();
-        } catch (Exception ex) {
-            log.error("Error occured while executing R cmd ", ex);
+        } catch (final Exception ex) {
+            REngineFacade.log.error("Error occured while executing R cmd ", ex);
             throw new REngineFacadeEvalException("Error occured while executing R cmd ", ex);
         }
     }
@@ -98,20 +103,24 @@ public class REngineFacade {
         //this.rEvalSync("plot(1:10,2:11)");
         this.rEvalAsync("10+2", new IREvalCallbackHandler() {
 
-            public void newREXP(REXP rExp) {
+            @Override
+			public void newREXP(final REXP rExp) {
                 System.out.println("[Async:]" + rExp);
             }
         });
         //this.rEngine.eval("plot(iris)", false);
     }
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         try {
-            REngineFacade rFacade = REngineFacade.getInstance();
+        	REngineFacade.log.info("R_HOME=" + System.getProperty("R_HOME"));
+        	REngineFacade.log.info("LD_LIBRARY_PATH="+System.getProperty("LD_LIBRARY_PATH"));
+        	REngineFacade.log.info("java.library.path=" + System.getProperty("java.library.path"));
+            final REngineFacade rFacade = REngineFacade.getInstance();
             rFacade.test();
             //rFacade.end();
-        } catch (Exception exc) {
-            log.error("An error occured", exc);
+        } catch (final Exception exc) {
+            REngineFacade.log.error("An error occured", exc);
             System.err.println("An erroc occured. See log for details.");
             System.exit(1);
         }
@@ -122,7 +131,8 @@ public class REngineFacade {
         private final String rCmd;
         private final IREvalCallbackHandler cbHandler;
 
-        private RCallable() {
+        @SuppressWarnings("unused")
+		private RCallable() {
             this(null, null);
         }
 
@@ -140,21 +150,22 @@ public class REngineFacade {
          *  returns the result. A callback handler, if registered,
          *  is called.
          *
-         *  @return the result; null if an error occured
+         *  @return the result; null if an error occurred
          */
-        public REXP call() {
+        @Override
+		public REXP call() {
             REXP result = null;
             try {
-                result = rEngine.eval(this.rCmd);
-            } catch (Exception exc) {
-                log.error("Error occured while executing R cmd", exc);
+                result = REngineFacade.this.rEngine.eval(this.rCmd);
+            } catch (final Exception exc) {
+                REngineFacade.log.error("Error occured while executing R cmd", exc);
             }
             try {
                 if (this.cbHandler != null) {
                     this.cbHandler.newREXP(result);
                 }
-            } catch (Exception exc) {
-                log.error("Error occured while notifying callback handler", exc);
+            } catch (final Exception exc) {
+                REngineFacade.log.error("Error occured while notifying callback handler", exc);
             }
             return result;
         }
@@ -165,32 +176,37 @@ class TextConsole implements RMainLoopCallbacks {
 
     private static final Log log = LogFactory.getLog(TextConsole.class);
 
-    public void rWriteConsole(Rengine re, String text, int oType) {
-        log.info(text);
+    @Override
+	public void rWriteConsole(final Rengine re, final String text, final int oType) {
+        TextConsole.log.info(text);
     }
 
-    public void rBusy(Rengine re, int which) {
-        log.info("rBusy(" + which + ")");
+    @Override
+	public void rBusy(final Rengine re, final int which) {
+        TextConsole.log.info("rBusy(" + which + ")");
     }
 
-    public String rReadConsole(Rengine re, String prompt, int addToHistory) {
+    @Override
+	public String rReadConsole(final Rengine re, final String prompt, final int addToHistory) {
         System.out.print(prompt);
         try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            String s = br.readLine();
-            return (s == null || s.length() == 0) ? s : s + "\n";
-        } catch (Exception e) {
-            log.error("jriReadConsole exception", e);
+            final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            final String s = br.readLine();
+            return ((s == null) || (s.length() == 0)) ? s : s + "\n";
+        } catch (final Exception e) {
+            TextConsole.log.error("jriReadConsole exception", e);
         }
         return null;
     }
 
-    public void rShowMessage(Rengine re, String message) {
-        log.info(message);
+    @Override
+	public void rShowMessage(final Rengine re, final String message) {
+        TextConsole.log.info(message);
     }
 
-    public String rChooseFile(Rengine re, int newFile) {
-        FileDialog fd = new FileDialog(new Frame(), (newFile == 0) ? "Select a file" : "Select a new file", (newFile == 0) ? FileDialog.LOAD : FileDialog.SAVE);
+    @Override
+	public String rChooseFile(final Rengine re, final int newFile) {
+        final FileDialog fd = new FileDialog(new Frame(), (newFile == 0) ? "Select a file" : "Select a new file", (newFile == 0) ? FileDialog.LOAD : FileDialog.SAVE);
         fd.show();
         String res = null;
         if (fd.getDirectory() != null) {
@@ -202,12 +218,15 @@ class TextConsole implements RMainLoopCallbacks {
         return res;
     }
 
-    public void rFlushConsole(Rengine re) {
+    @Override
+	public void rFlushConsole(final Rengine re) {
     }
 
-    public void rLoadHistory(Rengine re, String filename) {
+    @Override
+	public void rLoadHistory(final Rengine re, final String filename) {
     }
 
-    public void rSaveHistory(Rengine re, String filename) {
+    @Override
+	public void rSaveHistory(final Rengine re, final String filename) {
     }
 }
