64ad2a7f52f14aee664daa1eccde6af7f32ea156
Andre van Hoorn <avh@informatik.uni-kiel.de>
2011-05-13 15:09:24 +0200

    fixed start script

--- bin/slastic.sh
+++ bin/slastic.sh
@@ -7,7 +7,7 @@
 BINDIR=$(dirname $0)
 
 JAVAARGS="-Dlog4j.configuration=${BINDIR}/log4j.properties -Xms56m -Xmx1024m"
-MAINCLASSNAME=org.trustsoft.slastic.common.SLAsticStarter
+MAINCLASSNAME=org.trustsoft.slastic.plugins.starter.SLAsticStarter
 CLASSPATH=$(ls lib/*.jar | tr "\n" ":")$(ls dist/*.jar | tr "\n" ":")
 
 java ${JAVAARGS} -cp "${CLASSPATH}" ${MAINCLASSNAME} $*
