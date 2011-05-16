2e62a6efe8f417c14a6a0431b1496c8051af4718
Andre van Hoorn <avh@informatik.uni-kiel.de>
2011-05-16 10:14:31 +0200

    detailed log4j integration into examples (thanks Florian)

--- examples/Dummy-Configuration/bin/slastic.sh
+++ examples/Dummy-Configuration/bin/slastic.sh
@@ -9,13 +9,13 @@ BINDIR=$(dirname $0)
 L4JPROPS="${BINDIR}/log4j.properties"
 L4JPROPSEXAMPLE="${BINDIR}/log4j.properties.example"
 
-JAVAARGS="-Dlog4j.configuration=${BINDIR}/log4j.properties -Xms56m -Xmx1024m"
+JAVAARGS="-Dlog4j.configuration=${L4JPROPS} -Xms56m -Xmx1024m"
 MAINCLASSNAME=org.trustsoft.slastic.plugins.starter.SLAsticStarter
 CLASSPATH=$(ls lib/*.jar | tr "\n" ":")$(ls dist/*.jar | tr "\n" ":")
 
 echo
 echo "Reading log4j configuration from file ${L4JPROPS} ... "
-if [ ! -f "" ]; then
+if [ ! -f "${L4JPROPS}" ]; then
     echo "No log4j configuration. Using default. The file '${L4JPROPSEXAMPLE}' includes a template."
 fi
 echo
