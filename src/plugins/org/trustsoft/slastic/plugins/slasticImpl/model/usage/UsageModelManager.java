09a9e393479bfd198a31e8a7bf1eeb49c51d0710
Andre van Hoorn <avh@informatik.uni-kiel.de>
2011-10-13 10:29:31 +0200

    updating usage model (only missing thing: calling relationship)

--- src/plugins/org/trustsoft/slastic/plugins/slasticImpl/model/usage/UsageModelManager.java
+++ src/plugins/org/trustsoft/slastic/plugins/slasticImpl/model/usage/UsageModelManager.java
@@ -1,19 +1,14 @@
 package org.trustsoft.slastic.plugins.slasticImpl.model.usage;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.trustsoft.slastic.plugins.slasticImpl.model.AbstractModelManager;
 
-import de.cau.se.slastic.metamodel.componentAssembly.AssemblyComponentConnector;
+import de.cau.se.slastic.metamodel.componentAssembly.SystemProvidedInterfaceDelegationConnector;
 import de.cau.se.slastic.metamodel.monitoring.DeploymentComponentOperationExecution;
 import de.cau.se.slastic.metamodel.monitoring.MonitoringFactory;
-import de.cau.se.slastic.metamodel.typeRepository.Interface;
 import de.cau.se.slastic.metamodel.typeRepository.Operation;
 import de.cau.se.slastic.metamodel.typeRepository.Signature;
-import de.cau.se.slastic.metamodel.usage.AssemblyConnectorCallFrequency;
-import de.cau.se.slastic.metamodel.usage.CallingRelationship;
 import de.cau.se.slastic.metamodel.usage.OperationCallFrequency;
+import de.cau.se.slastic.metamodel.usage.SystemProvidedInterfaceDelegationConnectorFrequency;
 import de.cau.se.slastic.metamodel.usage.UsageFactory;
 import de.cau.se.slastic.metamodel.usage.UsageModel;
 
@@ -36,37 +31,82 @@ public class UsageModelManager extends AbstractModelManager<UsageModel> implemen
 	}
 
 	@Override
-	public void assemblyConnectorCall(final AssemblyComponentConnector assemblyConnector, final Signature signature) {
-		// TODO
+	public void incSystemProvidedInterfaceSignatureCallFreq(final SystemProvidedInterfaceDelegationConnector connector,
+			final Signature signature) {
+		SystemProvidedInterfaceDelegationConnectorFrequency freq = null;
+		
+		for (final SystemProvidedInterfaceDelegationConnectorFrequency freqTmp : super.getModel().getSystemProvidedInterfaceDelegationConnectorFrequencies()) {
+			if (freqTmp.getConnector().equals(connector) && freqTmp.getSignature().equals(signature)) {
+				// found the matching structure
+				freq = freqTmp;
+				break;
+			}
+			// else: continue
+		}
+		
+		if (freq == null) {
+			// no observations for connector signature, yet -> create and add
+			freq = UsageFactory.eINSTANCE.createSystemProvidedInterfaceDelegationConnectorFrequency();
+			freq.setConnector(connector);
+			freq.setSignature(signature);
+			super.getModel().getSystemProvidedInterfaceDelegationConnectorFrequencies().add(freq);
+		}
+		
+		// Now, increment frequency:
+		final long oldFrequency = freq.getFrequency();
+		freq.setFrequency(oldFrequency+1);
+	}
+
+	@Override
+	public void incOperationCallFreq(final Operation operation, final long frequency) {
+		OperationCallFrequency freq = null;
+		
+		for (final OperationCallFrequency freqTmp : super.getModel().getOperationCallFrequencies()) {
+			if (freqTmp.getOperation().equals(operation)) {
+				// found the matching structure
+				freq = freqTmp;
+				break;
+			}
+		}
+		
+		if (freq == null) {
+			freq = UsageFactory.eINSTANCE.createOperationCallFrequency();
+			freq.setOperation(operation);
+			super.getModel().getOperationCallFrequencies().add(freq);
+		}
+		
+		// Now, increment frequency
+		final long oldFrequency = freq.getFrequency();
+		freq.setFrequency(oldFrequency+1);
 	}
 	
-	/**
-	 * Operation call frequencies (operation id x frequency) 
-	 */
-	private final Map<Long, OperationCallFrequency> opCallFrequencies = new HashMap<Long, OperationCallFrequency>();
-	
-	/**
-	 * Calling relationships among {@link Operation}s and {@link Signature}s of an {@link Interface}.   
-	 */
-	private final Map<Long, CallingRelationship> opCallRelationships = new HashMap<Long, CallingRelationship>();
-	
-	/** Frequencies of calls to {@link Signature}s of an {@link AssemblyComponentConnector} */
-	private final Map<Long, AssemblyConnectorCallFrequency> connectorCallFrequencies = new HashMap<Long, AssemblyConnectorCallFrequency>();
-
-	/**
-	 * 
-	 * 
-	 * @param operation
-	 * @param iface
-	 * @param signature
-	 * @return
-	 */
-	public long opCallRelationshipHashCode(final Operation operation, final Interface iface, final Signature signature) {
-		final int prime = 31;
-		long result = 1;
-		result = prime * result + operation.getId();
-		result = prime * result + iface.getId();
-		result = prime * result + signature.getId();
-		return result;
-	}	
+//	/**
+//	 * Operation call frequencies (operation id x frequency) 
+//	 */
+//	private final Map<Long, OperationCallFrequency> opCallFrequencies = new HashMap<Long, OperationCallFrequency>();
+//	
+//	/**
+//	 * Calling relationships among {@link Operation}s and {@link Signature}s of an {@link Interface}.   
+//	 */
+//	private final Map<Long, CallingRelationship> opCallRelationships = new HashMap<Long, CallingRelationship>();
+//	
+//	/** Frequencies of calls to {@link Signature}s of an {@link AssemblyComponentConnector} */
+//	private final Map<Long, AssemblyConnectorCallFrequency> connectorCallFrequencies = new HashMap<Long, AssemblyConnectorCallFrequency>();
+//
+//	/**
+//	 * 
+//	 * 
+//	 * @param operation
+//	 * @param iface
+//	 * @param signature
+//	 * @return
+//	 */
+//	public long opCallRelationshipHashCode(final Operation operation, final Interface iface, final Signature signature) {
+//		final int prime = 31;
+//		long result = 1;
+//		result = prime * result + operation.getId();
+//		result = prime * result + iface.getId();
+//		result = prime * result + signature.getId();
+//		return result;
+//	}	
 }
