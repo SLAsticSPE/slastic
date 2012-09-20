f61541553bd368f44f5757a0c706d7ab854dcaee
Andre van Hoorn <avh@informatik.uni-kiel.de>
2012-09-20 16:20:01 +0200

    #33: property to (de)activate trace and usage model reconstruction

--- src/plugins/kieker/tools/slastic/plugins/slasticImpl/ModelUpdater.java
+++ src/plugins/kieker/tools/slastic/plugins/slasticImpl/ModelUpdater.java
@@ -16,6 +16,8 @@
 
 package kieker.tools.slastic.plugins.slasticImpl;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import kieker.tools.slastic.common.event.IObservationEvent;
 import kieker.tools.slastic.control.components.events.IEvent;
@@ -29,10 +31,13 @@ import kieker.tools.slastic.plugins.slasticImpl.control.modelUpdater.traceRecons
  */
 public class ModelUpdater extends AbstractModelUpdaterComponent {
 
-	// private static final Log LOG = LogFactory.getLog(ModelUpdater.class);
+	private static final Log LOG = LogFactory.getLog(ModelUpdater.class);
 
-	// TODO: turn into property
-	private static final long TRACE_DETECTION_TIMEOUT_MILLIS = 1000;
+	private static final String PROP_NAME_TRACE_USAGE_RECONSTR_ENABLED = "traceUsageReconstructionEnabled";
+	private static final String PROP_NAME_TRACE_DETECTION_TIMEOUT_MILLIS = "traceDetectionTimeoutMillis";
+
+	private volatile boolean traceUsageReconstructionEnabled = true;
+	private volatile long traceDetectionTimeOutMillis = 1000;
 
 	/**
 	 * Will be initialized in {@link #execute()}.
@@ -58,6 +63,12 @@ public class ModelUpdater extends AbstractModelUpdaterComponent {
 
 	@Override
 	public boolean init() {
+		this.traceUsageReconstructionEnabled =
+				Boolean.parseBoolean(super.getInitProperty(PROP_NAME_TRACE_USAGE_RECONSTR_ENABLED, Boolean.toString(this.traceUsageReconstructionEnabled)));
+		if (this.traceUsageReconstructionEnabled) {
+			this.traceDetectionTimeOutMillis =
+					Long.parseLong(super.getInitProperty(PROP_NAME_TRACE_DETECTION_TIMEOUT_MILLIS, Long.toString(this.traceDetectionTimeOutMillis)));
+		}
 		return true;
 	}
 
@@ -67,15 +78,20 @@ public class ModelUpdater extends AbstractModelUpdaterComponent {
 		 * Constructs a {@link TraceReconstructor} which registers itself as a
 		 * subscriber to the {@link EPServiceProvider}.
 		 */
-		this.traceReceiver = new TraceReconstructor(this.getParentControlComponent().getEPServiceProvider(), TRACE_DETECTION_TIMEOUT_MILLIS);
+		if (this.traceUsageReconstructionEnabled) {
+			LOG.info("Trace and usage model reconstruction enabled with timeout (millis): " + this.traceDetectionTimeOutMillis);
 
-		/*
-		 * Constructs a {@link UsageAndAssemblyModelUpdater} which registers
-		 * itself as a subscriber to the {@link EPServiceProvider}.
-		 */
-		this.usageAndAssemblyModelUpdater =
-				new UsageAndAssemblyModelUpdater(this.getParentControlComponent().getEPServiceProvider(), (ModelManager) this.getModelManager());
+			this.traceReceiver = new TraceReconstructor(this.getParentControlComponent().getEPServiceProvider(), this.traceDetectionTimeOutMillis);
 
+			/*
+			 * Constructs a {@link UsageAndAssemblyModelUpdater} which registers
+			 * itself as a subscriber to the {@link EPServiceProvider}.
+			 */
+			this.usageAndAssemblyModelUpdater =
+					new UsageAndAssemblyModelUpdater(this.getParentControlComponent().getEPServiceProvider(), (ModelManager) this.getModelManager());
+		} else {
+			LOG.info("Trace and usage model reconstruction disabled");
+		}
 		return true;
 	}
 
